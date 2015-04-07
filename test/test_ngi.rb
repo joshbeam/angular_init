af605f91897a8b379aa3944ccfc7a6c31b46a5d6
Joshua Beam <joshua.a.beam@gmail.com>
2015-04-07 17:58:07 -0500

    using minitest instead

--- test/test_ngi.rb
+++ test/test_ngi.rb
@@ -1,44 +1,59 @@
 require_relative '../lib/ngi'
-require 'test/unit'
+require 'minitest/autorun'
 require 'json'
 
-# Tests for Ngi
-class TestNgi < Test::Unit::TestCase
-  CURRENT_DIR = File.dirname(__FILE__)
+describe Ngi do
+  DIR = File.dirname(__FILE__)
 
-  def configure_init
-    path = CURRENT_DIR + '/sup/test.config.json'
-    configure = Ngi::Delegate::Configure.new(path)
+  describe Ngi::Delegate::Configure do
+    before do
+      @path = "#{DIR}/sup/test.config.json"
+      @config = Ngi::Delegate::Configure.new(@path)
+    end
 
-    r = {
-      path: path,
-      configure: configure
-    }
-
-    r
-  end
-
-  def test_configure_file
-    c = configure_init
-
-    assert_equal(c[:configure].file, IO.read(c[:path]))
-  end
-
-  def test_configure_json
-    c = configure_init
-    file = c[:configure].file
-    json_file = c[:configure].from_json
-
-    assert_equal(json_file, JSON.parse(file))
-  end
-
-  def test_configure_run
-    c = configure_init
-    file = c[:configure].file
-    Ngi::Delegate::Configure.run(write: false, file_path: c[:path])
-
-    # assert that even if we change a property in the config file,
-    # it never gets written UNLESS in Configure#run, write == true
-    assert_equal(file, IO.read(c[:path]))
+    describe 'when initialized' do
+      it 'must read the file given from the path' do
+        @config.file.must_equal IO.read(@path)
+      end
+    end
   end
 end
+# class TestNgi < Test::Unit::TestCase
+#   CURRENT_DIR = File.dirname(__FILE__)
+
+#   def configure_init
+#     path = CURRENT_DIR + '/sup/test.config.json'
+#     configure = Ngi::Delegate::Configure.new(path)
+
+#     r = {
+#       path: path,
+#       configure: configure
+#     }
+
+#     r
+#   end
+
+#   def test_configure_file
+#     c = configure_init
+
+#     assert_equal(c[:configure].file, IO.read(c[:path]))
+#   end
+
+#   def test_configure_json
+#     c = configure_init
+#     file = c[:configure].file
+#     json_file = c[:configure].from_json
+
+#     assert_equal(json_file, JSON.parse(file))
+#   end
+
+#   def test_configure_run
+#     c = configure_init
+#     file = c[:configure].file
+#     Ngi::Delegate::Configure.run(write: false, file_path: c[:path])
+
+#     # assert that even if we change a property in the config file,
+#     # it never gets written UNLESS in Configure#run, write == true
+#     assert_equal(file, IO.read(c[:path]))
+#   end
+# end
